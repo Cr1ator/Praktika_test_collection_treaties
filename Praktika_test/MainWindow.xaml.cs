f9d9ece886f0242766510fc10fdb4044bbcc3b8d
ddoshechko <ddoshechko@bs-solutions.by>
2023-01-07 22:21:22 +0300

    left menu

--- Praktika_test/MainWindow.xaml.cs
+++ Praktika_test/MainWindow.xaml.cs
@@ -1,28 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Praktika_test
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e) 
+        { 
+            if (e.ChangedButton == MouseButton.Left) 
+            {
+                this.DragMove();
+            }
+        }
+
+        private bool IsMaximized = false;
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) 
+        { 
+            if (e.ClickCount == 2)
+            {
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1080;
+                    this.Height = 720;
+
+                    IsMaximized= false;
+                }
+                else
+                {
+                    this.WindowState= WindowState.Maximized;
+
+                    IsMaximized= true;
+                }
+            }
         }
     }
 }
